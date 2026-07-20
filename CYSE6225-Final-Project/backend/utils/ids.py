7f55978e9d4c435c43e57734203474207f2218e1
yeyeyuyin <133222226+yeyeyuyin@users.noreply.github.com>
2026-07-21 05:48:14 +0800

    Delete CYSE6225-Final-Project directory

--- CYSE6225-Final-Project/backend/utils/ids.py
+++
@@ -1,5 +0,0 @@
-import uuid
-
-
-def new_id() -> str:
-    return uuid.uuid4().hex
--- CYSE6225-Final-Project/backend/wsgi.py
+++
@@ -1,4 +0,0 @@
-"""Entry point for Gunicorn on EC2:  gunicorn -w 3 -b 0.0.0.0:8000 wsgi:app"""
-from app import create_app
-
-app = create_app()
